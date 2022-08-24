c89b87129abadbacbdf990ce1c169a8f0c4b6c23
mar <marilirulita@gmail.com>
2022-08-23 22:21:50 -0700

    add a session example in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,16 @@ class PagesController < ApplicationController
   end
   
   def hello
+    session[:current_name] = 'Mar y sol'
     @greeting = 'Hello, World!'
     @info = 'This is a new info'
-    @answer = false
+    @answer = true
     @isTrue = 'This variable is true'
     @isFalse = 'This variable is false'
+    @other_name = session[:current_name]
+  end
+
+  def set_session
+    session[:current_name] = 'Mar y sol'
   end
 end
