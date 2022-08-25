37eea1631ebe26033ccc5c63f32671fdedf3630f
mar <marilirulita@gmail.com>
2022-08-24 18:31:01 -0700

    add devise and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   def self.recent_posts(author)
     Text.where(author_id: author.id).limit(3).order(created_at: :asc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#hello'
   get '/greeting', to: 'custom_pages#hello'
 
