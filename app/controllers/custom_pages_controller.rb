0db8f3f59553bfad95e7fc1a94d379e4d6492891
mar <marilirulita@gmail.com>
2022-10-26 19:55:59 -0700

    delete unecesary controller

--- app/controllers/custom_pages_controller.rb
+++ app/controllers/custom_pages_controller.rb
@@ -1,7 +1,7 @@
 class CustomPagesController < ApplicationController
   
   def hello
-    render "greet_the_world"
-   # render :layout => 'mailer'
+    # render "greet_the_world"
+   render :layout => 'mailer'
   end
 end
